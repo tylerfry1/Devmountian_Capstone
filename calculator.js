815acb8fcd3f351cd318e0e649fac4912aaf84fd
tylerfry1 <tyler.fry1@outlook.com>
2023-10-22 17:57:00 -0600

    Revised Commit

--- calculator.js
+++ calculator.js
@@ -82,9 +82,9 @@ class Calculator {
     }
   }
 }
-const numberButtons = document.querySelectorAll("[data-number");
+const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
-const equalsButton = document.querySelector("[data-equals");
+const equalsButton = document.querySelector("[data-equals]");
 const deleteButton = document.querySelector("[data-delete]");
 const allClearButton = document.querySelector("[data-all-clear]");
 const previousOperandTextElement = document.querySelector(
